f342ca5f409a01d1deb1dbdad873f862a0a1158c
Herman Anez <herman.anez@alumnos.uneatlantico.es>
2021-08-14 21:01:37 +0200

    -

--- redisServer/index.js
+++ redisServer/index.js
@@ -10,13 +10,25 @@ const server = express();
 
 async function buscarElemento(elemento){
     let valor;
-    
-    await redisClient.get(elemento, async (err,data)=>{
+
+    let promise = new Promise((resolve, reject) => {
+        redisClient.get(elemento, (e, data) => {
+          if(e){
+            reject(e);
+          }
+          resolve(data);
+        });
+      });
+
+
+    /*await redisClient.get(elemento, async (err,data)=>{
         if(err) throw err;
         else if(data) {console.log(await data); valor=data;}
         else  {valor=0}
         }
-    )
+    )*/
+
+    valor = await promise.then((x)=>{return x})
     console.log(valor);
     return valor
 
