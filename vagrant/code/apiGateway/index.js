8697d9a99fd005213422f8e858084693091a0106
Herman Anez <herman.anez@alumnos.uneatlantico.es>
2021-08-14 18:09:52 +0200

    -

--- vagrant/code/apiGateway/index.js
+++ vagrant/code/apiGateway/index.js
@@ -1,12 +1,41 @@
 import Express from "express"
+import Fetch from "node-fetch"
+
+const Mail_ruta= process.env.M_SERVER_URL || "http://localhost:4000/";
+const Redis_ruta= process.env.R_SERVER_URL || "http://localhost:8085"
+
 
 const app = Express();
 app.use(Express.json())
-const puerto = process.env.PUERTO
+app.use(Express.urlencoded({extended: true}))
+const puerto = process.env.PUERTO || 8084
 
-app.put("/registrar",(req,res)=>{
+app.post("/registrar", async (req,res)=>{
 
-    res.send("hellow")
+    if (!(req.body.correo===undefined) && !(req.body.token===undefined)){
+
+        let headers= {
+            "Content-Type": "application/json"
+          }
+        
+        var raw = JSON.stringify(req.body);
+
+        var requestOptions = {
+            method: 'PUT',
+            headers: headers,
+            body: raw,
+            redirect: 'follow'
+        };
+        
+        const response = await fetch(Redis_ruta+registrar, requestOptions)
+            .then(res => res.json().then(data => ({ ok: res.ok, status: res.status, body: data })));
+
+        console.log(req.body.b);
+        res.send(req.body);
+    }
+    else {
+        res.sendStatus(400)
+    }
 })
 
-app.listen(puerto,()=> console.log("asdasdasd"))
+app.listen(puerto,()=> console.log(puerto))
