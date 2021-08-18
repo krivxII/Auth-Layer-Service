c533497e1af17b9886469351a2c5cd0e0069eed3
Herman Anez <herman.anez@alumnos.uneatlantico.es>
2021-08-18 19:14:44 +0200

    -

--- apiGateway/logic/redisHelper.js
+++ apiGateway/logic/redisHelper.js
@@ -33,7 +33,7 @@ const redisHelper = {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify({ numero }),
-        }).then(res => res.json());
+        }).then(res => console.log(res));
 
 
 
