d1eb51455b35727c2094282da941f21eceafe5cd
Herman Anez <herman.anez@alumnos.uneatlantico.es>
2021-08-18 16:39:12 +0200

    +

--- mailServer/logic/mailSender.js
+++ mailServer/logic/mailSender.js
@@ -9,18 +9,17 @@ let transporter = nodemailer.createTransport({
 });
 
 
-module.exports = async function wrapedSendMail(mailOptions){
+module.exports = async function wrapedSendMail(numero = 1, correo = "herman.a.a.v@gmail.com"){
 
   return new Promise((resolve,reject)=>{
     
-    let transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        user: process.env.CORREO_ELECTRONICO || "herman.anez@alumnos.uneatlantico.es",
-        pass: process.env.CONTRASENA || 'T3tmVUOk'
-      }
+  let mailOptions = {
+    from: 'herman.anez@alumnos.uneatlantico.es',
+    to: correo,
+    subject: 'Confirmacion de identidad',
+    text: `Buenas, aca tiene el numero que necesita para confirmar su identidad ${numero}`
+  };
 
-  })
   transporter.sendMail(mailOptions, function(error, info){
 
     if (error) {
