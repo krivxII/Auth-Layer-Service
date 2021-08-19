31f849e9fd31df238e0bc13beecdf18696d1f4bd
Herman Anez <herman.anez@alumnos.uneatlantico.es>
2021-08-19 14:37:44 +0200

    refactorizacion completa

--- apiGateway/src/servidor/servidor.js
+++ apiGateway/src/servidor/servidor.js
@@ -1,14 +1,14 @@
-import Express from "express"
+import express from "express"
 import cors from "cors"
 import rutasService from "../router/rutasService.js"
 
 
 const servidor = {
      puerto : process.env.PORT || 8085,
-     app : Express(),
+     app : express(),
       startServer(){
-         this.app.use(Express.json())
-         this.app.use(Express.urlencoded({ extended: true }))
+         this.app.use(express.json())
+         this.app.use(express.urlencoded({ extended: true }))
          this.app.use(cors())
          this.app.use("/",rutasService)
          this.app.listen(this.puerto, () => console.log(this.puerto))
