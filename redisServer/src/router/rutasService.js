1f690678c2c92fca7228d9c81483d25f53c2dc4c
Herman Anez <herman.anez@alumnos.uneatlantico.es>
2021-08-22 16:02:11 +0200

    -

--- redisServer/src/router/rutasService.js
+++ redisServer/src/router/rutasService.js
@@ -7,3 +7,22 @@ router.post("/registrar", middlewares.registrarSchema,controllers.registarContro
 router.post("/validar", middlewares.validarSchema,controllers.validarControler)
 
 module.exports = router
+
+
+const controllers = require("../controllers/controllers.js")
+const middlewares = require("../middleware/middlewares.js")
+const express = require("express")
+
+const erouter =  express.Router();
+
+const router ={
+
+    crearRouter() {
+        erouter.post("/registrar", middlewares.registrarSchema,controllers.registarControler)
+        erouter.post("/validar", middlewares.validarSchema,controllers.validarControler)
+        return erouter
+    }
+}
+
+
+module.exports = router
