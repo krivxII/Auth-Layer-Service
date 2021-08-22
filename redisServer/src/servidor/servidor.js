f581f141e350c37b659aaa6ff11c45b8141fc92f
Herman Anez <herman.anez@alumnos.uneatlantico.es>
2021-08-22 16:00:08 +0200

    -

--- redisServer/src/servidor/servidor.js
+++ redisServer/src/servidor/servidor.js
@@ -7,8 +7,8 @@ const app = express();
 const servidor = {
      puerto : process.env.PORT || 8085,
       startServer(){
-         app.use(Express.json())
-         app.use(Express.urlencoded({ extended: true }))
+         app.use(express.json())
+         app.use(express.urlencoded({ extended: true }))
          app.use(cors())
          app.use("/",rutasService.crearRouter())
          app.listen(this.puerto, () => console.log(this.puerto))
