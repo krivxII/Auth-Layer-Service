6ab8ef4ab4b2f6b51914798998c7583094f14d0b
Herman Anez <herman.anez@alumnos.uneatlantico.es>
2021-08-22 15:57:07 +0200

    -

--- redisServer/src/index.js
+++ redisServer/src/index.js
@@ -1,21 +1,13 @@
-const express = require("express");
-const cors = require("cors");
-const controllers = require("./controllers/controllers.js");
-const middlewares =require("./middleware/middlewares.js");
-
-const port = process.env.PORT || 80
-const app = express();
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(cors());
+const servidor = require("./servidor/servidor.js")
 
 
-function startServer(){
+const main = {
 
-    app.post("/registrar", middlewares.registrarSchema,controllers.registarControler)
-    app.post("/validar", middlewares.validarSchema,controllers.validarControler)
-    app.listen(port);
-    
-}
-
-startServer();
+    startApp(){
+         servidor.startServer();
+    }
+   
+   
+   }
+   
+   main.startApp();
--- redisServer/src/servidor/servidor.js
+++ redisServer/src/servidor/servidor.js
@@ -1,13 +1,19 @@
-const servidor = require("./servidor/servidor.js.js")
+const express = require("express")
+const cors = require("cors")
+const rutasService = require("../router/rutasService.js")
+
+const app = express();
+
+const servidor = {
+     puerto : process.env.PORT || 8085,
+      startServer(){
+         app.use(Express.json())
+         app.use(Express.urlencoded({ extended: true }))
+         app.use(cors())
+         app.use("/",rutasService.crearRouter())
+         app.listen(this.puerto, () => console.log(this.puerto))
+     }
+}
 
 
-const main = {
-
-    startApp(){
-         servidor.startServer();
-    }
-   
-   
-   }
-   
-   main.startApp();
+module.exports = servidor
